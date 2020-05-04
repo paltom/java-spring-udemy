90a3dd2f0fe336b93024347a057aa69ef6f4d237
paltom <tom0palka@gmail.com>
2020-05-04 16:12:11 +0200

    Activity #5 - DI with Annotations

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/FileFortuneService.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileFortuneService implements FortuneService {
+
+    private List<String> fortunes;
+
+    private Random random = new Random();
+
+    public FileFortuneService(@Value("${fortunes.file}") Path file) {
+        fortunes = readFortunesFile(file);
+    }
+
+    private List<String> readFortunesFile(Path file) {
+        try (BufferedReader reader = Files.newBufferedReader(file)) {
+            return reader.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("Cannot read fortunes file " + file.toAbsolutePath().toString() + ": " + e.getMessage());
+        }
+        return List.of("");
+    }
+
+    @Override
+    public String getFortune() {
+        return fortunes.get(random.nextInt(fortunes.size()));
+    }
+
+}
