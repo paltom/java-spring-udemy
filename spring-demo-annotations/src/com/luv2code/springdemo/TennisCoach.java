1a3382db37429432bd45e84b4fcfb3c3257e9aab
paltom <tom0palka@gmail.com>
2020-05-05 16:40:03 +0200

    PostConstruct and PreDestroy annotations

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,18 +1,29 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Autowired
-    @Qualifier("fileFortuneService")
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("Tennis Coach: inside init method");
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("TennisCoach: inside destroy method");
+    }
+    
     // @Autowired
     // public TennisCoach(FortuneService fortuneService) {
     //     this.fortuneService = fortuneService;
