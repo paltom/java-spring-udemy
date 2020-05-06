23e2f3df0f7494757e00a87c259a48dc17a94079
paltom <tom0palka@gmail.com>
2020-05-06 13:28:58 +0200

    Manual Bean configuration in Java source code

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config file
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                SportConfig.class)) {
+
+            // get the bean from spring container
+            Coach coach = applicationContext.getBean("swimCoach", Coach.class);
+
+            // call a method on the bean
+            System.out.println(coach.getDailyWorkout());
+            System.out.println(coach.getDailyFortune());
+        } catch (BeansException e) {
+            // TODO: handle exception
+        }
+    }
+}
