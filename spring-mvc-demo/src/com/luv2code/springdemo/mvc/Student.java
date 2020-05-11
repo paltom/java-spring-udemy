08c185fbbe39269734ead0381372d1a09df63ce2
paltom <tom0palka@gmail.com>
2020-05-11 15:10:05 +0200

    Form tags - selections

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -1,12 +1,21 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
     
     private String firstName;
     private String lastName;
+    private String country;
+    private LinkedHashMap<String, String> countryOptions;
 
     public Student() {
-        
+        countryOptions = new LinkedHashMap<>();
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("US", "USA");
     }
 
     /**
@@ -35,6 +44,28 @@ public class Student {
      */
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    /**
+     * @return String return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    /**
+     * @return LinkedHashMap<String, String> return the countryOptions
+     */
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
     }
 
 }
