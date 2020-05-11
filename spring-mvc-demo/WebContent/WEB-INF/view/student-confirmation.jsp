31a3088e05844b60ddb53f77ed135d6667aee371
paltom <tom0palka@gmail.com>
2020-05-11 15:46:49 +0200

    Form tags - radio buttons

--- spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -11,6 +11,9 @@
     The student is confirmed: ${student.firstName} ${student.lastName}
     <br><br>
     Country: ${student.country}
+    
+    <br><br>
+    Favorite Language: ${student.favoriteLanguage}
 </body>
 
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -8,6 +8,7 @@ public class Student {
     private String lastName;
     private String country;
     private LinkedHashMap<String, String> countryOptions;
+    private String favoriteLanguage;
 
     public Student() {
         countryOptions = new LinkedHashMap<>();
@@ -66,6 +67,28 @@ public class Student {
      */
     public LinkedHashMap<String, String> getCountryOptions() {
         return countryOptions;
+    }
+
+
+    /**
+     * @param countryOptions the countryOptions to set
+     */
+    public void setCountryOptions(LinkedHashMap<String, String> countryOptions) {
+        this.countryOptions = countryOptions;
+    }
+
+    /**
+     * @return String return the favoriteLanguage
+     */
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    /**
+     * @param favoriteLanguage the favoriteLanguage to set
+     */
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 
 }
