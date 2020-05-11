2cf89054a958d4f3bb947e2e89c173d6487cf8ce
paltom <tom0palka@gmail.com>
2020-05-11 13:36:12 +0200

    Spring MVC Form tags - model attribute

--- spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -11,6 +11,6 @@
 
 <hr>
 
-<a href="hello/showForm">Hello World form</a>
+<a href="student/showForm">Student form</a>
 </body>
 </html>
---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,40 @@
+package com.luv2code.springdemo.mvc;
+
+public class Student {
+    
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+        
+    }
+
+    /**
+     * @return String return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return String return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
