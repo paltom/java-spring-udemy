e0a4e65d295f1d79167c8a18c1c2bca63964248a
paltom <tom0palka@gmail.com>
2020-05-20 15:46:34 +0200

    Customize error message on type mismatch

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -14,9 +14,10 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName;
 
+    @NotNull(message="is required")
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp="^[0-9a-zA-Z]{5}", message="only 5 chars/digits")
     private String postalCode;
@@ -50,16 +51,16 @@ public class Customer {
     }
 
     /**
-     * @return int return the freePasses
+     * @return Integer return the freePasses
      */
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
     /**
      * @param freePasses the freePasses to set
      */
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
