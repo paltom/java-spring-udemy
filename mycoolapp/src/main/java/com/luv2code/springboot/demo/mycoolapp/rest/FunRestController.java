df3bd5b1ea28473ad4ef3c5160baaebea53eaeaa
paltom <tom0palka@gmail.com>
2020-06-23 13:57:53 +0200

    Use application.properties inside code
    Not SpringBoot specific

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,6 +2,7 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teamInfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     // expose "/" that return "Hello World"
     @GetMapping("/")
